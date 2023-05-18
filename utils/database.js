34e948524dec4f234a369d06de29825d359e3260
safin777 <sayedislamsafin7770555@gmail.com>
2023-05-18 20:00:34 +0600

    git new deployment

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI.toString(), {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
